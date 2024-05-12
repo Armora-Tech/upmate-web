4fbbfa9f2ed775ab94e8669e4406a6fba74b84d5
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2024-05-12 18:47:08 +0700

    style: customize the display in section1 on the home page

--- src/components/pages/home/section1.tsx
+++ src/components/pages/home/section1.tsx
@@ -5,7 +5,7 @@ import splashScreenImg from "../../../assets/images/splash_screen.png";
 export function Section1() {
   const { t } = useTranslation();
   return (
-    <div className="h-2/3 min-w-full flex lg:flex-row flex-col md:space-x-10 my-5 relative px-5 -z-20">
+    <div className="h-2/3 min-w-full flex lg:flex-row flex-col md:space-x-10 my-10 relative px-5 -z-20">
       <div className="lg:w-1/2 w-full flex justify-center items-center">
         <div data-aos="fade-up" className="lg:w-full md:w-1/2 w-full bg-textBlack flex justify-center overflow-hidden pt-5 custom-border">
           <img src={splashScreenImg} className="section1-img" alt="splash_screen" />
